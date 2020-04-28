845475a66364fb7581f9ac7e355dd90cbe3a0090
jonathan <gaertnerjonathan@gmail.com>
2020-04-28 15:05:31 +0200

    Hopefully finished now testing

--- PicOrga.py
+++ PicOrga.py
@@ -3,6 +3,7 @@ from optparse import OptionParser
 import os
 import shutil
 import json
+import sqlite3
 
 
 # Parsing the arguments for the directory of unsorted pictures and the directory of the sorted.
@@ -22,6 +23,16 @@ parser.add_option("-p", "--only-pictures", dest="pics_only", action="store_true"
 s_dir = options.s_dir
 d_dir = options.d_dir
 
+# Datenbank um Duplikate zu indentifizieren.
+if os.path.isfile(d_dir + "pics.db"):
+    database = sqlite3.connect(d_dir + "pics.db")
+    database_cursor = database.cursor()
+else:
+    database = sqlite3.connect(d_dir + "pics.db")
+    database_cursor = database.cursor()
+    database_cursor.execute("CREATE TABLE pics_sum (sum TEXT)")
+    database.commit()
+
 pic_types = (".png", ".jpeg", ".jpg", ".gif", ".webp", ".tiff", ".psd", ".raw", ".bmp", ".heif", ".indd", ".jp2",
              ".svg", ".ai", ".eps", ".pdf", ".jpe", ".jif", ".jfif", ".jfi", ".tif", ".arw", ".cr2", ".k25", ".dib",
              ".ind", ".indt", ".j2k", ".jpf", ".jpx", ".jpm", ".mj2", ".svgz")
@@ -36,9 +47,23 @@ def search_dir(path):
         dir_content = os.listdir(path)
     for content in dir_content:
         if os.path.isfile(path + content):
-            # Getting the Metainformation needed to get the age of the file
-            infos = json.loads(os.popen("mediainfo " + path + content + " -f --Output=JSON").read())
-            print(infos["media"]["track"][0]["File_Modified_Local"])
+            # Getting the Meta Information needed to get the age of the file
+            infos = json.loads(os.popen("mediainfo '" + path + content + "' -f --Output=JSON").read())
+            date = infos["media"]["track"][0]["File_Modified_Date_Local"]
+            year = date[:4] + "/"
+            month = date[5:7] + "/"
+            file_name = date[8:10] + "-" + date[-8:] + "." + content.split(".")[-1]
+            md5sum = os.popen("md5sum " + path + content).read().split("  ")[0]
+            database_cursor.execute("SELECT sum FROM pics_sum where sum='" + md5sum + "'")
+            if not database_cursor.fetchall():
+                database_cursor.execute("INSERT INTO pics_sum VALUES ( '" + md5sum + "' ) ")
+                database.commit()
+                if os.path.isfile(s_dir + year + month + file_name):
+                    i = 0
+                    while os.path.isfile(s_dir + year + month + file_name + i):
+                        i += 1
+                    shutil.copyfile(path + content, d_dir + year + month + file_name + i)
+                shutil.copyfile(path + content, d_dir + year + month + file_name + i)
             if options.remove:
                 os.remove(path + content)
         else:
@@ -48,3 +73,6 @@ def search_dir(path):
 
 
 search_dir(s_dir)
+
+database.commit()
+database.close()
